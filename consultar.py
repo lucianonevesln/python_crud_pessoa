c6f24769171a6040677a2dddd0e53b3bc9d550cc
Alther Alves <para.alves@gmail.com>
2022-04-17 16:39:48 -0700

    refact consultar.py

--- consultar.py
+++ consultar.py
@@ -28,16 +28,20 @@ def _select_sql(raw_sql)
     conn.close()
     return result
 
+
 def consultar_pessoas2():
-    autores = _select_sql('select * from python_crud_pessoas.pessoas;')
+    autores = _select_sql('SELECT * FROM PYTHON_CRUD_PESSOAS.PESSOAS;')
     return jsonify(autores)
 
 
 def consultar_contatos2():
-    nomes = _select_sql('select * from python_crud_pessoas.contatos;')
+    nomes = _select_sql('SELECT * FROM PYTHON_CRUD_PESSOAS.CONTATOS;')
     return jsonify(nomes)
 
 
 def consultar_pessoas_contatos2():
-    nomes = _select_sql('select p.nome, p.cpf, c.telefone, c.email from python_crud_pessoas.pessoas as p inner join python_crud_pessoas.contatos as c on c.id_pessoas = p.id;')
+    SQL_STATEMENT = 'SELECT P.NOME, P.CPF, C.TELEFONE, C.EMAIL '\
+        'FROM PYTHON_CRUD_PESSOAS.PESSOAS AS P '\
+        'INNER JOIN PYTHON_CRUD_PESSOAS.CONTATOS AS C ON C.ID_PESSOAS = P.ID;'
+    nomes = _select_sql(SQL_STATEMENT)
     return jsonify(nomes)
